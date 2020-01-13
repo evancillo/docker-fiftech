2939e9179afb0b9e2dae82c03e5eec2b283e7bfa
Sergio Aguilera <sergio.aguilera@cursor.cl>
2020-01-13 11:25:12 -0300

    * se expone rutas de modulo User

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 
 
-const characterModule = require('./modules/character/character.routes');
+const characterRoutes = require('./modules/character/character.routes');
+const userRoutes = require('./modules/user/user.routes');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,7 +13,8 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/character', characterModule);
+app.use('/api/character', characterRoutes);
+app.use('/api/user', userRoutes);
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
